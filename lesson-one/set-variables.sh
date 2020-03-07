d3d52c9464fc080a051c4c294f0db3d84bed63bf
Amauri De Souza Teixeira <ht3000699@HTO.IFSP.EDU.BR>
2020-03-06 22:04:13 -0300

    set-variables

--- lesson-one/set-variables.sh
+++ lesson-one/set-variables.sh
@@ -7,4 +7,13 @@ soma=$(expr $num1 + $num2)
 
 echo "A soma: $soma"
 
+echo "Digite um numero 1: "
+read num1
+echo "Digite um numero 2: "
+read num2
+
+multi=$(expr $num1 \* $num2)
+echo "A multiplicacao: $multi"
+
+
 exit 0
