fa2ecc12b489243c101a557b0677151ff74de82b
Amauri De Souza Teixeira <ht3000699@HTO.IFSP.EDU.BR>
2020-03-06 21:55:22 -0300

    lesson-one

---
+++ lesson-one/read-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Usuario do Sistema: $USERNAME"
+echo "Diretorio: $HOME"
+
+nome="Amauri Souza"
+echo "Meu nome é $nome"
+
+exit 0
---
+++ lesson-one/set-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+num1=10
+num2=40
+
+soma=$(expr $num1 + $num2)
+
+echo "A soma: $soma"
+
+exit 0
