6a369f9d92abbfacf1037a5855883135960ff817
Amauri de Souza Teixeira <amauri2015gg@gmail.com>
2020-03-08 19:43:53 -0300

    exercises 7 - list 4

---
+++ lesson-one/exercises/exe7.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# conf_rede IP MASCARA GATEWAY DNS1 DNS2
+conf_rede () {
+
+    # VALIDACAO DE PARAMS - NAO FUNCIONANDO
+    # if [ -n $1 || -n $2 || -n $3 || -n $4 ] ; then
+    #     echo "Alguns do parametros é vazio/nulo"
+    #     exit 1
+    # fi
+    
+    cd $HOME
+    pwd
+
+    cd etc/netplan
+
+    if [ -f config.yaml ] ; then
+        rm -R config.yaml
+    fi
+
+    ls
+
+    touch config.yaml
+    echo "network:
+            version: 2
+            ethernets: 
+                enp0s3:
+                    addresses: [$1]
+                    dhcp4: false
+                    gateway4: $2
+                    nameservers:
+                        addresses: [$3,$4]
+                    " >> config.yaml
+
+
+}
+
+conf_rede $1 $2 $3 $4
+
+exit 0
