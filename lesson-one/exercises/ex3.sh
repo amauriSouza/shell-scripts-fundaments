98a5f4e0afe6a9d18d8afebed03f629b4d9c02f2
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-03-08 16:30:09 -0300

    Exercise 3

--- lesson-one/exercises/ex3.sh
+++ lesson-one/exercises/ex3.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 echo "Digite o 1º numero: "
+read num1
+
 echo "Digite o 2º numero: "
+read num2
+
+echo "Digite o 3º numero: "
+read num3
+
+echo "Digite o 4º numero: "
+read num4
+
+media=$((($num1 + $num2 + $num3 + $num4)/4))
+
+echo "A média $media"
