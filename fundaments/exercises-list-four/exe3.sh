cb09d5172997c01d70a0a147add088c7cb02d0ab
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-03-13 20:29:10 -0300

    refactory exe3

--- fundaments/exercises-list-four/exe3.sh
+++ fundaments/exercises-list-four/exe3.sh
@@ -12,7 +12,7 @@ read num3
 echo "Digite o 4º numero: "
 read num4
 
-media=$((($num1 + $num2 + $num3 + $num4)/4))
+soma=$(($num1 + $num2 + $num3 + $num4))
 
-echo "A média $media"
+echo "A media: $(($soma/4))"
 exit 0
