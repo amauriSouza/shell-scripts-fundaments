33cf94eb442473c7b24963e1267186ddf6ed54e9
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-03-13 20:29:23 -0300

    exercise 8

---
+++ fundaments/exercises-list-four/exe8.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+calc_media(){
+    if [ ! $1 ] || [ ! $2 ] ; then
+        echo "Valores de entrada estão invalidos"
+        exit 0
+    fi
+soma=0
+    for (( i = $1 ; i < $2 ; i = i + 1 ))
+    do
+        if [ $(($i%2)) -eq '0' ] ; then
+            soma=$(expr $soma + $i)
+            let cont++
+            
+        fi
+    done
+
+    
+
+echo "SOMAAAAA: $soma"
+echo "A média dos numeros pares e: $(expr $soma / $cont)";
+}
+
+calc_media $1 $2
+
+exit 0
